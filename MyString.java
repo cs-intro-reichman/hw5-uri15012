4904fc9ae126bed426102b15817ff7cad0b3a501
uri15012 <uri.herszenhaut01@post.runi.ac.il>
2024-12-12 18:20:55 +0200

    q1

--- MyString.java
+++ MyString.java
@@ -1,3 +1,5 @@
+
+
 /**
  * A library of string functions.
  */
@@ -20,8 +22,15 @@ public class MyString {
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+        // nao precisa pegar os argumentos so no main que precisa 
+
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch){
+                counter++; 
+            }
+        }
+        return counter;
     }
 
     /** Returns true if str1 is a subset string str2, false otherwise
@@ -34,11 +43,21 @@ public class MyString {
      * @param str1 - a string
      * @param str2 - a string
      * @return true is str1 is a subset of str2, false otherwise
-     */
+     */// זה כמו מוכל
     public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
-        return false;
-    }
+        
+        for (int i = 0; i < str1.length(); i++) { // letras do str1 existe no str2
+            char  ch = str1.charAt(i); // pega a letra str1 
+            int numOfChInStr1 = countChar(str1, ch); // conta quantas vezes minha letra ta aqui
+            int numOfChInStr2 = countChar (str2,ch);
+            if (numOfChInStr1 > numOfChInStr2){ // se eu colocasse <= e true eu teria que continuar analisando assim eu vejo so uma vez 
+                return false;
+            }
+                
+            }
+            return true;
+        
+}
 
     /** Returns a string which is the same as the given string, with a space
      * character inserted after each character in the given string, except
@@ -49,8 +68,16 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+        String newStr = "";
+        int n = 0;
+        for (int i = 0; i < str.length(); i++){
+            if(i == str.length() -1){ // se for a ultima linha roda sem espaco
+                newStr += str.charAt(i);
+            }else{
+                newStr += str.charAt(i) + " ";
+            }
+        }
+        return newStr;
     }
   
     /**
@@ -64,10 +91,16 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
-    }
 
+        String str = "";
+        for (int i = 0; i <= n ; i++) {
+            char randomChar = (char) ('a' + (int) (Math.random()*26)); //'a' ponto de inicio ele sabe automaticamente comecar do a 
+            str += randomChar;
+            
+        }
+        return str;
+    }
+    
     /**
      * Returns a string consisting of the string str1, minus all the characters in the
      * string str2. Assumes (without checking) that str2 is a subset of str1.
@@ -78,10 +111,19 @@ public class MyString {
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
-    }
 
+        String finalStr = str1;
+
+        for (int i = 0; i < str2.length(); i++) {
+            char ch = str2.charAt(i); 
+            finalStr = finalStr.replaceFirst(Character.toString(ch), ""); //muda a primeira vez que aparece a letra na string
+        }
+
+        return finalStr;
+    }
+            
+        
+  
     /**
      * Returns a string consisting of the given string, with the given 
      * character inserted randomly somewhere in the string.
